98eb7d273480268572ce5308465c4db05290efe5
aliasbsv <aliasbsv@gmail.com>
2024-10-17 17:59:42 +0200

    Your commit message

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -11,7 +11,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = express.Router();
 const jsonParser = express.json();
 
-//Створюємо 2 маршрути для GET-запитів
+//Створюємо маршрути для GET-запитів
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
